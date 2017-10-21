90482385881ce5f44c3cc3a7484f146b59fa3820
tiamatt <samira.yusifova@gmail.com>
2017-10-21 13:38:01 -0400

    change assets path for some images

--- src/app/customized-item/topping-box/topping-box.component.ts
+++ src/app/customized-item/topping-box/topping-box.component.ts
@@ -14,7 +14,7 @@ export class ToppingBoxComponent implements OnInit {
 
   ngOnInit() {
     // this.imageUrl = "../../../assets/toppingImages/"+ this.topping.toppingCategoryId + "_" + this.topping.id + ".png";
-    this.imageUrl = "./assets/toppingImages/"+ this.topping.toppingCategoryId + "_" + this.topping.id + ".png";
+    this.imageUrl = "../assets/toppingImages/"+ this.topping.toppingCategoryId + "_" + this.topping.id + ".png";
   }
 
 }
